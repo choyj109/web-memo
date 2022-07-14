09c5a5dab12d79b5a0526d9cab3708bb21f85deb
choyj109 <choyj109@gmail.com>
2022-07-14 09:20:27 +0900

    merge main2

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { useDispatch } from "react-redux";
 import { deleteList } from "./../store/list/listSlice";
@@ -11,31 +10,15 @@ const List = ({ id, content }) => {
   };
   return (
     <div>
-      <h3>{id}</h3>
-      <p>{content}</p>
+      <div>
+        <h3>{id}</h3>
+        <p>{content}</p>
+      </div>
+      <button type="button" onClick={onDelete}>
+        삭제
+      </button>
     </div>
   );
-=======
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { deleteList } from './../store/list/listSlice';
-
-const List = ({id,content}) => {
-    const dispatch = useDispatch();
-    const onDelete = (e) => {
-        e.preventDefault();
-        dispatch(deleteList(id));
-    }
-    return (
-        <div>
-            <div>
-                <h3>{id}</h3>
-                <p>{content}</p>
-            </div>
-            <button type='button' onClick={onDelete}>  삭제 </button>
-        </div>
-    );
->>>>>>> 7fdaafcbed148b0b3380a5caf09e753b2a87fb5d
 };
 
 export default List;
