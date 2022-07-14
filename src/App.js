23ac887f89d27d2c0e1340e5d99c0695325bc358
choyj109 <choyj109@gmail.com>
2022-07-14 09:53:08 +0900

    modify listSlice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-<<<<<<< HEAD
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
 import { getList, addList } from "./store/list/listSlice";
 import List from "./components/List";
-import { useEffect, useState } from "react";
 
 function App() {
   const dispatch = useDispatch();
-  const listData = useSelector((state) => state.list);
+  const listdata = useSelector((state) => state.list);
   const [listValue, setListValue] = useState("");
   useEffect(() => {
     dispatch(getList());
@@ -20,65 +19,23 @@ function App() {
       setListValue("");
     }
   };
-  const onDelete = (e) => {
-    e.preventDefault();
-  };
-
   return (
     <div className="App">
       <form onSubmit={onCreate}>
-        <h1>{listData.message}</h1>
+        <h1>{listdata.message}</h1>
         <div>
-          {listData.data.map((ele) => (
+          {listdata.data.map((ele) => (
             <List key={ele.id} id={ele.id} content={ele.content} />
           ))}
-          {/* 데이터의 갯수만큼 map 돌리기 */}
         </div>
         <div>
-          <input type="text" value="입력된 글자" />
+          <input
+            type="text"
+            onChange={(e) => setListValue(e.target.value)}
+            value={listValue}
+          />
           <button type="submit">목록추가</button>
         </div>
-=======
-import './App.css';
-import {useSelector ,  useDispatch } from "react-redux";
-import { useEffect,useState} from "react";
-import { getList,addList } from './store/list/listSlice';
-import List from './components/List';
-
-function App() {
-  const dispatch = useDispatch();
-  const listdata = useSelector((state) => state.list);
-  const [listValue, setListValue] = useState('')
-  useEffect(() => {
-    dispatch(getList())
-    },[])
-const onCreate =(e) =>{
-  e.preventDefault();
-  if(listValue){
-    const newList = {content:listValue}  
-    dispatch(addList(newList))
-    setListValue('')
-  }
-}
-  return (
-    <div className="App">
-      <form onSubmit={onCreate}>
-         <h1>{listdata.message}</h1>
-         <div>
-            {
-              listdata.data.map((ele)=> <List key={ele.id} id={ele.id} content={ele.content} />)
-
-            }
-         </div>
-         <div>
-           <input 
-           type="text"
-           onChange={(e)=>setListValue(e.target.value)}
-           value={listValue}
-           />
-           <button type='submit'>목록추가</button>
-         </div>
->>>>>>> 7fdaafcbed148b0b3380a5caf09e753b2a87fb5d
       </form>
     </div>
   );
